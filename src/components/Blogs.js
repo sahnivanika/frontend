f6e59e1c70c58b61a222f57d694cf2f2119afdda
Vanika Sahni <141622433+sahnivanika@users.noreply.github.com>
2024-11-22 16:09:05 +0530

    Update Blogs.js

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,28 +1,41 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import axios from "axios";
 import Blog from './Blog';
+
 const Blogs = () => {
-   const [blogs,setBlogs]=useState();
-  const sendRequest = async()=>{
-    const res = await axios.get("https://backend-a0y9.onrender.com/api/blog").catch(err=>console.log(err));
-    const data = await res.data;
-    return data;
+  const [blogs, setBlogs] = useState();
+
+  const sendRequest = async () => {
+    try {
+      const res = await axios.get("https://backend-a0y9.onrender.com/api/blog");
+      const data = await res.data;
+      return data;
+    } catch (err) {
+      console.log(err);
+      return null;
+    }
   };
-  useEffect(()=> {
-   sendRequest().then((data)=>setBlogs(data.blogs));
-  },[]);
+
+  useEffect(() => {
+    sendRequest().then((data) => setBlogs(data?.blogs || []));
+  }, []);
+
   console.log(blogs);
-    return (
+
+  return (
     <div>
-      {blogs && blogs.map((blog,index)=> (
+      {blogs && blogs.map((blog, index) => (
         <Blog
-        isUser = {localStorage.getItem("userId")===blog.user._id} 
-        title={blog.title} description={blog.description} 
-        imageURL={blog.image} 
-        userName={blog.user.name} />
+          key={index} // Ensure each child has a unique key
+          isUser={localStorage.getItem("userId") === blog.user._id}
+          title={blog.title}
+          description={blog.description}
+          imageURL={blog.image || undefined} // Pass only if image is provided
+          userName={blog.user.name}
+        />
       ))}
     </div>
-    );
-}
+  );
+};
 
 export default Blogs;
